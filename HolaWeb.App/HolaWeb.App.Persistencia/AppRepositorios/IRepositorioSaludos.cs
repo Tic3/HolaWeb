1f95d54f87d111ee6c53b0fffc42cff7c1542b3f
Tic3 <misionlostic3@gmail.com>
2021-10-12 09:32:39 -0500

    primercargue

---
+++ HolaWeb.App/HolaWeb.App.Persistencia/AppRepositorios/IRepositorioSaludos.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using HolaWeb.App.Dominio;
+
+namespace HolaWeb.App.Persistencia.AppRepositorios
+{
+    public interface IRepositorioSaludos
+    {
+        IEnumerable<Saludo> GetAll();
+        IEnumerable<Saludo> GetSaludosPorFiltro(string filtro);
+        Saludo GetSaludoPorId(int saludoId);
+        Saludo Update (Saludo saludoActualizado);
+        Saludo Add(Saludo nuevoSaludo);
+        
+    }
+}
